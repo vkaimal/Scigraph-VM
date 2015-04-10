ea96f3c051284535f07be0426d2db86a40caf42a
Kabenla Armah <Kabenla@prometsource.com>
2015-04-09 22:08:12 -0500

    added options to deploy script to run build and start service

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,57 @@ usage()
     -h  Show this message
     -u  Updates the current branch of the SciGraph repository or clones the SciGraph repository if no SciGraph folder is found
     -b  Deploy (checkout) a specified branch.
+    -g  Generates Neo4j graph from ontology with a given configuration.yml file
+    -r 	Run service ontology store service using with a given configuration.yml file
     -i  Gets information about available branches
 EOF
+}
+
+function generate_neo4j_graph(){
+  if [[ ! -z $runontservice ]]
+  then
+    echo "Please run the deploy script with the -g option before the -r option"
+    exit 1
+  fi
+
+	if [[ -d 'SciGraph/SciGraph-core' ]]
+	then
+		pushd SciGraph/SciGraph-core
+		if [[ -e ../../build_configurations/$1 ]]
+		then
+			#mvn exec:java -Dexec.mainClass=\"edu.sdsc.scigraph.owlapi.loader.BatchOwlLoader\" -Dexec.args=\"-c ../../build_configurations/$1\"
+			echo "starting build from $1 configuration"
+		else
+			echo "$1 not found in build_configurations folder"
+			exit 1
+		fi
+	else
+		echo "Your SciGraph folder is incomplete.  Please delete the SciGraph folder and run the deploy script with the -u parameter"
+		exit 1
+	fi
+}
+
+function start_ontology_service(){
+	if [[ ! -z $genneo4jconfig ]]
+	then
+		popd
+	fi
+
+	if [[ -d 'SciGraph/SciGraph-services' ]]
+	then
+		cd SciGraph/SciGraph-services
+		if [[ -e ../../run_configurations/$1 ]]
+		then
+			#mvn exec:java -Dexec.mainClass=\"edu.sdsc.scigraph.services.MainApplication\" -Dexec.args=\"server ../../run_configurations/$1\"
+			echo "running service from $1 configuration"
+		else
+			echo "$1 not found in run_configurations folder"
+			exit 1
+		fi
+	else
+		echo "Your SciGraph folder is incomplete.  Please delete the SciGraph folder and run the deploy script with the -u parameter"
+		exit 1
+	fi
 }
 
 function update_scigraph(){
@@ -69,7 +118,7 @@ function show_branches(){
   fi
 }
 
-while getopts ":huib:" OPTION
+while getopts ":hug:r:ib:" OPTION
 do
   case $OPTION in
     h)
@@ -79,6 +128,14 @@ do
     u)
       update_scigraph
       ;;
+    g)
+      genneo4jconfig=$OPTARG
+      generate_neo4j_graph $genneo4jconfig
+      ;;
+    r)
+	  runontservice=$OPTARG
+	  start_ontology_service $runontservice
+	  ;;
     b)
       checkoutbranch=$OPTARG
       ;;
@@ -93,11 +150,11 @@ do
 done
 
 
-if [[ ! -z $checkoutbranch ]]
-then
-  echo "we will checkout and refactor all dependencies" $checkoutbranch
-  checkout_scigraph_branch $checkoutbranch
-else
-  usage
-  exit 1
-fi
+#if [[ ! -z $checkoutbranch ]]
+#then
+ # echo "we will checkout and refactor all dependencies" $checkoutbranch
+  #checkout_scigraph_branch $checkoutbranch
+#else
+ # usage
+  #exit 1
+#fi
