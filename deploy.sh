98ddf9772b3bee3cfc1f5dbe89a175c7ffed553e
Kabenla Armah <Kabenla@prometsource.com>
2015-04-20 20:54:07 -0500

    use logging option when using screen, and name the ontology server window

--- deploy.sh
+++ deploy.sh
@@ -333,7 +333,7 @@ function start_ontology_service(){
         echo "screen not found....installing screen"
         sudo apt-get install screen
       fi
-			screen -d -m mvn exec:java -Dexec.mainClass="edu.sdsc.scigraph.services.MainApplication" -Dexec.args="server ../../run_configurations/$1"
+			screen -L -S onto -d -m mvn exec:java -Dexec.mainClass="edu.sdsc.scigraph.services.MainApplication" -Dexec.args="server ../../run_configurations/$1"
 			echo "The ontology server has been setup on a detached screen."
       echo "To get back to the terminal running the server process (for example if you which to stop the server) execute 'screen -r' in the vagrant box"
 		else
